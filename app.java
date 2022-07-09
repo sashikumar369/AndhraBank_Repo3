91b16b0736c1b7f2e74a9de1d3ace29ba11e9715
sashikumar369 <sashikumar369@gmail.com>
2022-07-08 21:35:26 -0400

    app.java file has been modified by developer2

--- app.java
+++ app.java
@@ -24,3 +24,10 @@ class Main {
     }
   }
 }
+#feature 302 changes by developer2
+
+public static void sampleMethod(int a, int b) throws ArithmeticException{
+        System.out.println("Hello, this is sample method");
+        int c = a/b; //  int c = 4/2 which gets calculated as 2 
+        System.out.println("c:"+c); // c: 2
+   }
